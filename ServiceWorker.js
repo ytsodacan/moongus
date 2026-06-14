d119720aa94760b3967b096b7733e677613de27d
SillyProotSoda <103301261+ytsodacan@users.noreply.github.com>
2026-06-14 11:40:12 +1000

    goongus

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "XeraGamesLLC-Among Us-2019.2.15";
+const contentToCache = [
+    "Build/v1fix.loader.js",
+    "Build/v1fix.framework.js",
+    "Build/v1fix.data",
+    "Build/v1fix.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
